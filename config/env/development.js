6c38c0b255ef02df5939c10fd3bfe398b77dd16a
Bryan Tanner <btanne6@yahoo.com>
2015-01-14 18:16:12 +0000

    init

---
+++ config/env/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Development configuration options
+    db: 'mongodb://localhost/mean-book',
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: '830534673675651',
+        clientSecret: '2b3c753ce033b92d1807095d9be765f2',
+        callbackURL: 'https://mean-stack-btanne6.c9.io/oauth/facebook/callback'
+    }
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var mongoose = require('./config/mongoose'),
+    express = require('./config/express'),
+    passport = require('./config/passport');    
+    
+var db = mongoose();
+var app = express();
+var passport = passport();
+
+var ip = process.env.IP || "0.0.0.0";
+var port = process.env.PORT || 3000;
+app.listen(port, ip);
+
+module.exports = app;
+
+console.log('Server running at https://mean-stack-btanne6.c9.io');
