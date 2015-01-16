2a4546b75f4c5e08b9185ca11d69381e155b42d2
Bryan Tanner <btanne6@yahoo.com>
2015-01-16 18:50:26 +0000

    removed mongodb data files to reduce size

---
+++ config/env/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+    db: 'mongodb://localhost/mean-book-test',
+    sessionSecret: 'testSessionSecret',
+    viewEngine: 'ejs',
+    facebook: {
+        clientID: '830534673675651',
+        clientSecret: '2b3c753ce033b92d1807095d9be765f2',
+        callbackURL: 'https://mean-stack-btanne6.c9.io/oauth/facebook/callback'
+    }
+};
